304d0f0fe284a6b41116c350372138ae5b5a80be
ndbaykus <ndbaykus@gmail.com>
2026-02-05 23:21:00 +0100

    Update RTC configuration with additional STUN servers

--- emotionDemo/face_demo.py
+++ emotionDemo/face_demo.py
@@ -9,7 +9,13 @@ from Model_architecture_Code import Block, ResNet
 
 # --- GOOGLE STUN SUNUCULARI (Bağlantı Sorununu Çözen Kısım) ---
 RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302"]}]}
+    {"iceServers": [
+        {"urls": ["stun:stun.l.google.com:19302"]},
+        {"urls": ["stun:stun1.l.google.com:19302"]},
+        {"urls": ["stun:stun2.l.google.com:19302"]},
+        {"urls": ["stun:stun3.l.google.com:19302"]},
+        {"urls": ["stun:stun4.l.google.com:19302"]},
+    ]}
 )
 
 EMOTION_LABELS = ["kızgın maş", "Iıııı", "Korkmuş faşat", "Mutlu Bun", "Kederli Ferot", "Çok İlginçmişş"]
